3a2ef4edb2be9afe4796af8b15b4f916c4f54b84
@farissahman1 <farissahman1@gmail.com>
2021-03-10 18:35:37 +0100

    finished project

---
+++ pages/about.js
@@ -0,0 +1,27 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sed
+          in harum labore impedit necessitatibus rerum hic officiis ullam vitae
+          id dicta, distinctio facilis quod ratione perferendis non est eveniet.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sed
+          in harum labore impedit necessitatibus rerum hic officiis ullam vitae
+          id dicta, distinctio facilis quod ratione perferendis non est eveniet.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
---
+++ pages/ninjas/[id].js
@@ -0,0 +1,39 @@
+// getStaticPaths tells next how many html pages need to be made based on our data
+export const getStaticPaths = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+  const paths = data.map((ninja) => {
+    return {
+      params: { id: ninja.id.toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+// for each of above html getted pages from getStaticPaths will run getStaticProps
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const data = await res.json();
+
+  return {
+    props: { ninja: data },
+  };
+};
+
+const Details = ({ ninja }) => {
+  return (
+    <div>
+      <h1>{ninja.name}</h1>
+      <p>{ninja.email}</p>
+      <p>{ninja.website}</p>
+      <p>{ninja.address.city}</p>
+    </div>
+  );
+};
+
+export default Details;
